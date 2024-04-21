565fdd5a045d27d78b454b2d0db6f6f7ee762b88
Kami <kamibusinessinquirers@gmail.com>
2024-04-21 23:05:22 +1000

    forgot a version teehee

--- clakr.rb
+++ clakr.rb
@@ -1,15 +1,15 @@
 class Clakr < Formula
-  desc "Description of Clakr"
-  homepage "https://github.com/SenpaiHunters/Clakr"
-  url "https://github.com/SenpaiHunters/Clakr/releases/latest/download/clakr.app.zip"
-  sha256 "SHA256 of the tarball"
-
-  def install
-    # Install or smth
-    bin.install "clakr"
+    desc "Description of Clakr"
+    homepage "https://github.com/SenpaiHunters/Clakr"
+    url "https://github.com/SenpaiHunters/Clakr/releases/latest/download/clakr.app.zip"
+    version "1.1"
+    sha256 "SHA256 of the tarball"
+  
+    def install
+      bin.install "clakr"
+    end
+  
+    test do
+      system "#{bin}/clakr", "--version"
+    end
   end
-
-  test do
-    system "#{bin}/clakr", "--version"
-  end
-end
