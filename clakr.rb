fe69cb15edd41de642b19d91116a806381ccea4f
Kami <kamibusinessinquirers@gmail.com>
2024-04-21 23:10:28 +1000

    do you want a sha?

--- clakr.rb
+++ clakr.rb
@@ -3,7 +3,7 @@ class Clakr < Formula
     homepage "https://github.com/SenpaiHunters/Clakr"
     url "https://github.com/SenpaiHunters/Clakr/releases/latest/download/clakr.app.zip"
     version "1.1"
-    sha256 "SHA256 of the tarball"
+    sha256 "a2c6d8622e520ee0b1c0a2b97b2eb1a5b850031cd43da41fb01bc34e10d0fde5"
   
     def install
       bin.install "clakr"
